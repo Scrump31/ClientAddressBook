54f4c7a0dcd1c0a7e838eede1eb14c5296198e11
Scottie Crump <scottiecrump@yahoo.com>
2017-08-09 23:08:29 -0400

    created templates for all views and css files

--- index.js
+++ index.js
@@ -4,11 +4,29 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static(`${__dirname}/public`));
+
 app.set('view engine', 'ejs');
 
 
 app.get('/', (req, res) => {
   res.render('login');
+});
+
+app.get('/clients', (req, res) => {
+  res.render('clients');
+});
+
+app.get('/add', (req, res) => {
+  res.render('add-client');
+});
+
+app.get('/edit', (req, res) => {
+  res.render('edit-client');
+});
+
+app.get('/logout', (req, res) => {
+  res.render('logout');
 });
 
 
